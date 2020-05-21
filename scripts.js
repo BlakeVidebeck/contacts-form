ba5a26befa08af3cef4f0b0d3892f61f8442c78a
BlakeVidebeck <blakevidebeck@gmail.com>
2020-05-21 12:46:18 +1000

    made scripts more readable

--- scripts.js
+++ scripts.js
@@ -10,10 +10,10 @@ let searchSection = document.querySelector('.search');
 let searchPara = document.createElement('p');
 
 let login = document.querySelector('.pwd');
-let loginButton = document.querySelector('.loginButton')
-let loginSection = document.querySelector('.login')
+let loginButton = document.querySelector('.loginButton');
+let loginSection = document.querySelector('.login');
 let loginPara = document.createElement('p');
-let append = document.querySelector('.append')
+let append = document.querySelector('.append');
 let password = 'happy';
 
 //phonebook 
@@ -35,11 +35,9 @@ function contactPush(){
     } else {
         addPara.textContent = 'contact added successfully'
         phonebook.push({name: newName.value, number: newNumber.value})
-        for(contact of phonebook){
-           console.log(contact)
            newName.value = ''
-           newNumber.value = ''
-    }
+           newNumber.value = '' //has to be after the push otherwise it push blank values
+    
     }
 }
 
@@ -55,7 +53,8 @@ for(let contact of phonebook){
     ]
 }
 }
-//login to see contacts
+
+//login function to see contacts
 function loginFunction(){
     if(login.value !== password){
         loginPara.textContent = 'Not the right password!';
@@ -64,28 +63,38 @@ function loginFunction(){
         login.focus();
     } else {
         loginPara.textContent = '';
-        logoutButton = document.createElement('button');
-        logoutButton.setAttribute('class', 'logoutButton');
-        logoutButton.textContent = 'logout';
+        login.value = '';
+        createLogoutButton();
         loginSection.removeChild(loginButton);
         loginSection.appendChild(logoutButton);
-        logoutButton.addEventListener('click', logOut);
-
-        for(let contact of phonebook){
-        loginPara = document.createElement('p');
-        loginPara.textContent = `${contact.name}: ${contact.number}`;
-        append.appendChild(loginPara);
-        login.value = '';
-    }
+        showContacts();
     }
 }
 
+//append the contacts to the page
+function showContacts(){
+    for(let contact of phonebook){
+        loginPara = document.createElement('p');
+        loginPara.textContent = `${contact.name}: ${contact.number}`;
+        append.appendChild(loginPara);
+    }
+}
+
+//creates logout button and add logout function to it
+function createLogoutButton(){
+    logoutButton = document.createElement('button');
+    logoutButton.setAttribute('id', 'btn');
+    logoutButton.textContent = 'logout';
+    logoutButton.addEventListener('click', logOut);
+}
+
+//reset the logged in features when logged out button is pushed
 function logOut(){
         loginPara.textContent = 'You have successfully logged out';
         loginSection.appendChild(loginPara);
         loginSection.removeChild(logoutButton);
         loginSection.appendChild(loginButton);
-        while (append.firstChild) {
+        while (append.firstChild) { //while append.firstChild is true removes all append.firstChild
             append.removeChild(append.firstChild);
         }
         loginButton.addEventListener('click', loginFunction);
